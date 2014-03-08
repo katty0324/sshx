e0feef77a0570110fcc7ecc87054a49cf8ec7608
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2014-03-09 03:59:02 +0900

    Concat configuration files

--- lib/sshx/cli.rb
+++ lib/sshx/cli.rb
@@ -8,23 +8,31 @@ module Sshx
 
 				Cli.init
 
-				if args.length == 0 then
+				if args.length == 0
 					puts 'sshx is just a wrapper of ssh.'
 					puts `ssh`
 				return
 				end
 
+				config_file_path = '/tmp/sshx_config'
+				Cli.make_temporary_config(config_file_path)
+
 				command = 'ssh'
 				args.each{|arg|
 					command << ' ' + arg.shellescape
 				}
-				
+				command << ' -F ' + config_file_path
+
 				system(command)
-				exit $?.exitstatus
+				status = $?.exitstatus
+
+				File.unlink(config_file_path)
+
+				exit status
 
 			end
 
-			def init
+			def init()
 
 				home_directory = File.expand_path('~')
 
@@ -32,6 +40,26 @@ module Sshx
 					Dir.mkdir(home_directory + '/.sshx')
 					FileUtils.cp(home_directory + '/.ssh/config', home_directory + '/.sshx/ssh_config')
 				end
+
+			end
+
+			def make_temporary_config(target_path)
+
+				home_directory = File.expand_path('~')
+
+				config = ''
+				Dir::foreach(home_directory + '/.sshx/') {|file_path|
+					if /^\./ =~ file_path
+					next
+					end
+					file = open(home_directory + '/.sshx/' + file_path)
+					config << file.read + "\n"
+					file.close
+				}
+
+				file = open(target_path, 'w')
+				file.write(config)
+				file.close
 
 			end
 
