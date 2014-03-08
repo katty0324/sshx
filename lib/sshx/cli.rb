ecf3140666d0bc2f1543d25a96cd3b08c9f923ac
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2014-03-09 03:31:40 +0900

    Wrap ssh command

--- lib/sshx/cli.rb
+++ lib/sshx/cli.rb
@@ -1,9 +1,38 @@
+require 'fileutils'
+require 'shellwords'
+
 module Sshx
 	module Cli
 		class << self
 			def start(args = ARGV)
-				puts "\e[36msshx\e[0m"
+
+				Cli.init
+
+				if args.length == 0 then
+					puts 'sshx is just a wrapper of ssh.'
+					puts `ssh`
+				return;
+				end
+
+				command = 'ssh'
+				args.each{|arg|
+					command << ' ' + arg.shellescape
+				}
+				puts `#{command}`
+
 			end
+
+			def init
+
+				home_directory = File.expand_path('~')
+
+				if !File.exist?(home_directory + '/.sshx')
+					Dir.mkdir(home_directory + '/.sshx')
+					FileUtils.cp(home_directory + '/.ssh/config', home_directory + '/.sshx/ssh_config')
+				end
+
+			end
+
 		end
 	end
 end
